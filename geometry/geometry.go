e3d9b7b7db33c4a38ee61f168041c86f2474acda
chirag <chiragsmehta007@gmail.com>
2024-09-11 15:12:25 +0530

    zero edge not allowed

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -1,5 +1,10 @@
 package geometry
 
-func FindVolume(n int) int{
-	return n*n*n
+import "errors"
+
+func FindVolume(n int) (int, error) {
+	if n != 0 {
+		return n * n * n, nil
+	}
+	return 0, errors.New("zero edge not allowed")
 }
